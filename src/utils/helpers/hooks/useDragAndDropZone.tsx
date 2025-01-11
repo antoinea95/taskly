78daa5ee1b0ea0ed9611491e871e65b696bdaf31
Antoine <102359550+antoinea95@users.noreply.github.com>
2025-01-11 14:38:39 +0100

    fix: correct hook to have a better handle of the drag'n'drop feature
    Add list handling + improve firebase call with optimistic UI update

--- src/utils/helpers/hooks/useDragAndDropZone.tsx
+++ src/utils/helpers/hooks/useDragAndDropZone.tsx
@@ -1,204 +1,204 @@
-import { DragEndEvent, DragStartEvent, DragOverEvent } from "@dnd-kit/core";
-import { useState } from "react";
-import { useQueryClient } from "@tanstack/react-query";
-import { TaskType } from "@/utils/types/tasks.types";
 import { FirestoreService } from "@/utils/firebase/firestore/firestoreService";
+import { BoardType } from "@/utils/types/boards.types";
 import { ListType } from "@/utils/types/lists.types";
+import { TaskType } from "@/utils/types/tasks.types";
+import { DragEndEvent, DragOverEvent, DragStartEvent } from "@dnd-kit/core";
+import { useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
 
-type DragAndDropZoneProps = {
-  lists?: ListType[];
-  boardId: string;
-};
 
 /**
- * Custom hook to handle drag-and-drop events for tasks.
- * This hook provides:
- * - Tracking of the active task during dragging.
- * - Managing list changes when a task is moved between lists.
- * - Sorting tasks within the same list or between multiple lists.
- * 
- * The hook uses optimistic updates, updating the UI before synchronizing the changes with Firestore.
- * 
- * @param {DragAndDropZoneProps} props - Properties containing the list of tasks and the board ID.
- * @returns Handlers for drag start, drag over, and drag end events, as well as the active task state.
- */
-export const useDragAndDropZone = ({ lists, boardId }: DragAndDropZoneProps) => {
-  const [activeTask, setActiveTask] = useState<TaskType | null>(null);
+ * Custom hook to manage drag-and-drop functionality for tasks and lists.
+ *
+ * @param {ListType[]} lists - The lists associated with the current board.
+ * @param {BoardType} board - The current board containing the lists.
+ *
+*   handleDragStart: (event: DragStartEvent) => Promise<void>,
+*   handleDragOver: (event: DragOverEvent) => Promise<void>,
+*   handleDragEnd: (event: DragEndEvent) => Promise<void>,
+*   activeItem: { item: TaskType | ListType; type: "task" | "list" } | null
+*/
+export const useDragAndDropZone = (lists: ListType[], board: BoardType) => {
   const queryClient = useQueryClient();
 
-  /**
-   * Retrieves task data to display in the DragOverlay.
-   * 
-   * @param {string} taskId - ID of the task being dragged.
+    // State to track the currently active item being dragged.
+  const [activeItem, setActiveItem] = useState<{ item: TaskType | ListType; type: "task" | "list" } | null>(null);
+
+   /**
+   * Handles the start of a drag operation.
+   *
+   * @param {DragStartEvent} event - The event triggered when dragging starts.
    */
-  const getActiveTask = async (taskId: string) => {
-    const task = await new Promise<TaskType>((resolve, reject) => {
-      const unsubscribe = FirestoreService.subscribeToDocument<TaskType>(
-        "tasks",
-        taskId,
-        (task) => {
-          if (task) {
-            resolve(task);
-          } else {
-            reject(new Error(`Task with ID ${taskId} not found`));
-          }
-        },
-        (error) => {
-          reject(new Error(`Error while getting document: ${error.message}`));
-        },
-      );
-      return () => unsubscribe;
-    });
-    setActiveTask(task);
+  const handleDragStart = async ({ active }: DragStartEvent) => {
+    const activeType = active.data.current?.type;
+    console.log(active);
+
+    if (activeType === "task") {
+      const task = await FirestoreService.fetchDoc<TaskType>("tasks", active.id.toString());
+      if (!task) return;
+      setActiveItem({ item: task, type: "task" });
+    } else {
+      const list = await FirestoreService.fetchDoc<ListType>("lists", active.id.toString());
+      if (!list) return;
+      setActiveItem({ item: list, type: "list" });
+    }
   };
 
-  /**
-   * Sets the active task for dragging.
-   * 
-   * @param {DragStartEvent} active - The drag start event object.
-   */
-  const handleDragStart = ({ active }: DragStartEvent) => {
-    getActiveTask(active.id.toString());
-  };
 
-  /**
-   * Handles list changes when a task is dragged over a different list.
-   * 
-   * @param {DragOverEvent} active - The drag over event object.
+   /**
+   * Handles the drag-over event, updating the local React Query cache
+   * and preparing the data for persistence in Firestore.
+   *
+   * @param {DragOverEvent} event - The event triggered when dragging over a droppable area.
    */
   const handleDragOver = async ({ active, over }: DragOverEvent) => {
-    if (!over || !lists) {
-      return;
-    }
+    if (!over) return;
 
-    const initialContainer = active.data.current?.sortable?.containerId;
-    const targetContainer = over.data.current?.sortable?.containerId;
+    const activeType = active.data.current?.type;
+    const overType = over.data.current?.type;
 
-    const targetType = over.data.current?.type;
+    if (activeType === "task") {
+      const overListId = overType === "list" ? over.id : over.data.current?.sortable?.containerId;
+      const activeListId = active.data.current?.sortable?.containerId;
 
-    if (!initialContainer || initialContainer === targetContainer) {
-      return;
-    }
+      // Récupérer les listes concernées
+      const overList = lists.find((list) => list.id === overListId);
+      const activeList = lists.find((list) => list.id === activeListId);
 
-    const overId = targetContainer ? targetContainer : over.id.toString();
+      // Vérification de la validité des listes
+      if (!activeList || !overList) return;
 
-    const oldList = lists.find((list) => list.id === initialContainer);
-    const newList = lists.find((list) => list.id === overId);
+      // Indices des tâches
+      const overIndex = over.data.current?.sortable?.index ?? 0;
+      const activeIndex = active.data.current?.sortable?.index ?? null;
 
-    if (oldList && newList && !newList.tasks.includes(active.id.toString())) {
-      const targetIndex =
-        targetType === "task"
-          ? over.data.current?.sortable?.index
-          : newList.tasks.length === 0
-            ? 0
-            : newList.tasks.length - 1;
+      if (activeList === overList) {
+        if (overIndex === null || activeIndex === null) return;
 
-      const sortedTask = [...newList.tasks];
-      sortedTask.splice(targetIndex, 0, active.id.toString());
+        // Reorganize tasks within the same list.
+        const updatedTasks = [...activeList.tasks];
+        const [movedTask] = updatedTasks.splice(activeIndex, 1);
+        updatedTasks.splice(overIndex, 0, movedTask);
 
-      const removedTask = oldList.tasks.filter(
-        (taskId) => taskId !== active.id
-      );
+        queryClient.setQueryData(["lists", board.id], (previousLists?: ListType[]) => {
+          if (!previousLists) return previousLists;
+          return previousLists.map((list) => (list.id === activeList.id ? { ...list, tasks: updatedTasks } : list));
+        });
+      } else {
+        if (overIndex === null) return;
 
-      // Optimistic update
-      queryClient.setQueryData(
-        ["lists", boardId],
-        (oldLists: ListType[] | undefined) => {
-          if (!oldLists) return oldLists;
+         // Update tasks in both the active list and the target list.
+        const updatedActiveListTasks = activeList.tasks.filter((taskId) => taskId !== active.id);
 
-          return oldLists.map((list) => {
-            if (list.id === newList.id) {
-              return { ...list, tasks: sortedTask };
-            } else if (list.id === oldList.id) {
-              return { ...list, tasks: removedTask };
-            } else {
-              return list;
+        const updatedOverListTasks = [...overList.tasks];
+        updatedOverListTasks.splice(overIndex, 0, active.id.toString());
+
+        queryClient.setQueryData(["lists", board.id], (previousLists?: ListType[]) => {
+          if (!previousLists) return previousLists;
+          return previousLists.map((list) => {
+            if (list.id === activeList.id) {
+              return { ...list, tasks: updatedActiveListTasks };
             }
-          });
-        }
-      );
-
-      // Firestore update
-      try {
-        await Promise.all([
-          FirestoreService.updateDocument<ListType>(
-            "lists",
-            { tasks: sortedTask },
-            newList.id
-          ),
-          FirestoreService.updateDocument<ListType>(
-            "lists",
-            { tasks: removedTask },
-            oldList.id
-          ),
-        ]);
-        queryClient.invalidateQueries({ queryKey: ["lists", boardId] });
-      } catch (error: any) {
-        queryClient.invalidateQueries({ queryKey: ["lists", boardId] });
-        throw new Error(error);
-      }
-    }
-  };
-
-  /**
-   * Handles task sorting within the same list after dragging ends.
-   * 
-   * @param {DragEndEvent} active - The drag end event object.
-   */
-  const handleDragEnd = async ({ active, over }: DragEndEvent) => {
-    if (!over || !lists || !active.data.current || !over.data.current) {
-      setActiveTask(null);
-      return;
-    }
-
-    const initialContainer = active.data.current.sortable?.containerId;
-    const targetContainer = over.data.current?.sortable?.containerId;
-
-    if (active.id === over.id || initialContainer !== targetContainer) {
-      setActiveTask(null);
-      return;
-    }
-
-    setActiveTask(null);
-
-    const targetIndex = over.data.current?.sortable?.index;
-    const listToUpdate = lists.find((list) => list.id === initialContainer);
-
-    if (listToUpdate) {
-      const sortedTask = [
-        ...listToUpdate.tasks.filter((id) => id !== active.id),
-      ];
-      sortedTask.splice(targetIndex, 0, active.id.toString());
-
-      // Optimistic update
-      queryClient.setQueryData(
-        ["lists", boardId],
-        (oldLists: ListType[] | undefined) => {
-          if (!oldLists) return oldLists;
-
-          return oldLists.map((list) => {
-            if (list.id === listToUpdate.id) {
-              return { ...list, tasks: sortedTask };
+            if (list.id === overList.id) {
+              return { ...list, tasks: updatedOverListTasks };
             }
             return list;
           });
-        }
-      );
+        });
 
-      // Firestore update
+
+        // Persist the changes in Firestore.
+        try {
+            await Promise.all([
+              FirestoreService.updateDocument<ListType>("lists", { tasks: updatedOverListTasks }, overList.id),
+              FirestoreService.updateDocument<ListType>("lists", { tasks: updatedActiveListTasks }, activeList.id),
+            ]);
+          } catch (error: any) {
+            queryClient.invalidateQueries({ queryKey: ["lists", board.id] });
+            throw new Error(error);
+          }
+      }
+    }
+
+    // Sort when the lists move
+    if (activeType === "list" && overType !== "task") {
+      // Find indexs
+      const activeIndex = board.lists.indexOf(active.id.toString());
+      const overIndex = board.lists.indexOf(over.id.toString());
+
+      // Copy board lists to handle sort
+      const sortedListsInBoard = [...board.lists];
+
+      // Delete active elements from is
+      sortedListsInBoard.splice(activeIndex, 1);
+
+      // Insérer l'élément actif à la nouvelle position
+      sortedListsInBoard.splice(overIndex, 0, active.id.toString());
+
+      queryClient.setQueryData(["board", board.id], (previousBoard?: BoardType) => {
+        if (!previousBoard) return previousBoard;
+        return { ...board, lists: sortedListsInBoard };
+      });
+    }
+  };
+
+
+    /**
+   * Handles the end of a drag operation, finalizing updates in Firestore.
+   *
+   * @param {DragEndEvent} event - The event triggered when dragging ends.
+   */
+  const handleDragEnd = async ({ active, over }: DragEndEvent) => {
+    if (!over) return;
+    setActiveItem(null);
+
+    const activeType = active.data.current?.type;
+    const overType = over.data.current?.type;
+
+
+    // Update lists ordrer in the board
+    if (activeType === "list" && overType !== "task") {
+
+      // Get the new board in Local query cache set in the dragover fn
+      const newBoard: BoardType | undefined = queryClient.getQueryData(["board", board.id]);
       try {
-        await FirestoreService.updateDocument<ListType>(
-          "lists",
-          { tasks: sortedTask },
-          listToUpdate.id
-        );
-        queryClient.invalidateQueries({ queryKey: ["lists", boardId] });
-      } catch (error: any) {
-        queryClient.invalidateQueries({ queryKey: ["lists", boardId] });
-        throw new Error(error);
+        if (!newBoard) {
+          queryClient.invalidateQueries({ queryKey: ["board", board.id] });
+          return;
+        }
+        await FirestoreService.updateDocument<BoardType>("boards", { lists: newBoard.lists }, board.id);
+      } catch (error) {
+        queryClient.invalidateQueries({ queryKey: ["board", board.id] });
+        console.error(`Error while updating the board ${board.id}`);
+        throw error;
+      }
+    }
+
+
+    // Update the tasks order in lists
+    if (activeType === "task") {
+      const activeListId = active.data.current?.sortable?.containerId;
+
+      const newLists: ListType[] | undefined = queryClient.getQueryData(["lists", board.id]);
+      if (!newLists) {
+        queryClient.invalidateQueries({ queryKey: ["lists", board.id] });
+      }
+
+      // Get the new list in Local query cache set in the dragover fn
+      const activeList = newLists?.find((list) => list.id === activeListId.toString());
+      try {
+        if (!activeList) {
+          queryClient.invalidateQueries({ queryKey: ["lists", board.id] });
+          return;
+        }
+        await FirestoreService.updateDocument<ListType>("lists", { tasks: activeList.tasks }, activeList.id);
+      } catch (error) {
+        queryClient.invalidateQueries({ queryKey: ["lists", board.id] });
+        console.error(`Error while updating the list ${activeList?.id}`);
+        throw error;
       }
     }
   };
 
-  return { handleDragStart, handleDragOver, handleDragEnd, activeTask };
+  return { handleDragStart, handleDragOver, handleDragEnd, activeItem };
 };
